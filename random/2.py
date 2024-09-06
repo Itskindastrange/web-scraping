03ac8748f854dfc4022afba72cad55a90a0a657a
abdullah <abdullahahmaddd789@gmail.com>
2024-09-06 16:50:13 +0500

    cookie clicker

---
+++ random/2.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+import time
+
+
+PATH = "C:\\Program Files (x86)\\chromedriver.exe"
+service = Service(executable_path=PATH)
+driver = webdriver.Chrome(service=service)
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+driver.implicitly_wait(15)
+lang = driver.find_element(By.ID,'langSelect-EN')
+lang.click()
+driver.implicitly_wait(15)
+
+
+cookie = driver.find_element(By.ID ,"bigCookie")
+#cookie_count = driver.find_elements(By.ID,"cookies")
+
+#items = [driver.find_element(By.ID,'productPrice' + str(i)) for i in range(1,-1,-1)]
+actions = ActionChains(driver)
+actions.click(cookie)
+
+for i in range(1000):
+    actions.perform()
+#     for element in cookie_count:
+#         count = int(element.text.split(" ")[0])
+#         for item in items:
+#             value = int(item.text)
+#             if value <= count:
+#                 upgrade_action = ActionChains(driver)
+#                 upgrade_action.move_to_element(item)
+#                 upgrade_action.click()
+#                 actions.perform()
+    
+
